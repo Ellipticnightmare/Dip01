8e90baf313533cba475cb524d2fade8cf30f504d
Ellipticnightmare <31974144+Ellipticnightmare@users.noreply.github.com>
2018-08-15 15:52:00 +1000

    Add files via upload

---
+++ Assets/Scripts/Gravity/GravityBody.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+
+    public GravityAttractor attractor;
+    private Transform myTransform;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        myTransform = transform;
+    }
+
+    void FixedUpdate()
+    {
+        if (attractor)
+        {
+            attractor.Attract(myTransform);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        GravityAttractor obj = col.GetComponent("GravityAttractor") as GravityAttractor;
+        if (obj)
+        {
+            attractor = obj;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        attractor = null;
+    }
+
+}
